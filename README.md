2be1dc422816c9000c6ec02cda75c8710faa0f12
Minh <hoaiminh94@gmail.com>
2020-03-09 15:23:56 +0700

    After Lession 8: Configuration Fluent API

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # ASP.Net Core 3.1 Project from TEDU
 ## Technonogies
 - ASP .NET Core 3.1
-Entity Framework Core 3.1
+- Entity Framework Core 3.1
+## Install Package
+- Microsoft.EntityFrameworkCore.SQLServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
 
 ## Youtube tutorial
 ## How to configure and run
---
+++ eShopSolution.Data/EF/eShopDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopSolution.Data.Entities;
+using eShopSolution.Data.Configurations;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            //base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+    }
+}
