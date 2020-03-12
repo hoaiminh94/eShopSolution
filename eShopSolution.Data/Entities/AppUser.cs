3baa172136e884c94c74e70e8aec2e808d85d7d5
Minh <hoaiminh94@gmail.com>
2020-03-12 09:40:44 +0700

    Create database first version

--- eShopSolution.Data/Entities/AppUser.cs
+++ eShopSolution.Data/Entities/AppUser.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace eShopSolution.Data.Entities
 {
---
+++ eShopSolution.Data/Entities/TestDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class TestDbContext
+    {
+        public String CreateDbContext()
+        {
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //  .SetBasePath(Directory.GetCurrentDirectory())
+            //  .AddJsonFile("appsettings.json")
+            //  .Build();
+
+            var s1 = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
+            var s2 = s1.AddJsonFile("appsettings.json").Build();
+
+
+            var ConnectionString = s2.GetConnectionString("eShopSolutionDb");
+
+            return ConnectionString;
+        }
+    }
+}
